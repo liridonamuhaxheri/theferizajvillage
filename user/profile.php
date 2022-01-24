1634c49bb1b3cf1199b0e9a4ab2db4c8961adf30
Liridona Muhaxheri <lm28887@seeu.edu.mk>
2022-01-24 19:19:43 +0100

    admin_panel

--- user/profile.php
+++ user/profile.php
@@ -113,8 +113,6 @@ body{
 </style>
  <?php 
  include('../db_connect.php');
- if (isset($_SESSION['user']))
- {
     $uid=$_SESSION["user"];
     $query="select * from users where uid=$uid"; 
     $elements=$conn->query($query);
@@ -256,5 +254,4 @@ body{
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<?php  include('../footer.php');}
-include('formlogin.html');?>
+<?php  include('../footer.php');?>
